504b44b5ff77cc4fc0a9804bf7957ee70b187123
black <black>
2025-02-19 22:12:01 +1100

    n

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -8,24 +8,24 @@ interface SettingsProps {
 
 export default ({ settings }: SettingsProps) => {
 
-   const searchUserMessages = async (user_id, guild_id, token) => {
-      const response = await fetch(`https://discord.com/api/v9/channels/${guild_id}/messages/search?author_id=${user_id}`, {headers: {"Authorization": token}})
-      const rejson = await response.json()
-      if (rejson.total_results > 0) {
-         settings.set('guild_list', settings.get('guild_list', '') + "," + guild_id)
-      }
-   }
+   // const searchUserMessages = async (user_id, guild_id, token) => {
+   //    const response = await fetch(`https://discord.com/api/v9/channels/${guild_id}/messages/search?author_id=${user_id}`, {headers: {"Authorization": token}})
+   //    const rejson = await response.json()
+   //    if (rejson.total_results > 0) {
+   //       settings.set('guild_list', settings.get('guild_list', '') + "," + guild_id)
+   //    }
+   // }
    
-   const listServersUserIsActiveIn = async () => {
-      const token = Token.getToken()
-      const user_id = settings.get('userid', '')
-      const response = await fetch(`https://discord.com/api/v9/users/@me/guilds`, {headers: {"Authorization": token}})
-      const rejson = await response.json()
-      const guild_ids: Array<number> = rejson.map(guild => guild.id)
-      for (const guild_id of guild_ids) {
-         await searchUserMessages(user_id, guild_id, token)
-      }
-   }
+   // const listServersUserIsActiveIn = async () => {
+   //    const token = Token.getToken()
+   //    const user_id = settings.get('userid', '')
+   //    const response = await fetch(`https://discord.com/api/v9/users/@me/guilds`, {headers: {"Authorization": token}})
+   //    const rejson = await response.json()
+   //    const guild_ids: Array<number> = rejson.map(guild => guild.id)
+   //    for (const guild_id of guild_ids) {
+   //       await searchUserMessages(user_id, guild_id, token)
+   //    }
+   // }
 
    const execute = () => {
       settings.set('pressed', 'yes')
