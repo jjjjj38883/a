727315d45d3ac594b45f9ae6995d8e9000cb8b0f
black <black>
2025-02-19 20:22:06 +1100

    new

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,19 +1,34 @@
-import { FormRow, FormSwitch } from 'enmity/components';
+import { FormCTAButton, FormInput, FormRow, FormSwitch } from 'enmity/components';
 import { SettingsStore } from 'enmity/api/settings';
 import { React } from 'enmity/metro/common';
+import { Token } from 'enmity/metro/common';
+
 
 interface SettingsProps {
    settings: SettingsStore;
 }
 
 export default ({ settings }: SettingsProps) => {
-   return <FormRow
-      label='Example Setting'
-      trailing={
-         <FormSwitch
-            value={settings.get('example', true)}
-            onValueChange={() => settings.toggle('example', true)}
-         />
-      }
-   />;
+
+   const searchUserMessages = async (user_id, guild_id) => {
+      const token = Token.getToken()
+   }
+   
+   const listServersUserIsActiveIn = () => {
+      
+   }
+   return (<>
+      <FormRow
+         label='Example Setting'
+         trailing={
+            <FormSwitch
+               value={settings.get('example', true)}
+               onValueChange={() => settings.toggle('example', true)}
+         />}
+      />
+      <FormInput value={settings.get('userid', '')} onTextChange={(text) => settings.set('userid', text)} />
+      <FormCTAButton onClick={listServersUserIsActiveIn()} />
+      <Text>{Token.getToken()}</Text>
+      {settings.get('server_list', '')}
+   </>)
 };
