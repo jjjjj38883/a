fbc79856eba7b785b58c88130b0bc82a11dcff1c
black <black>
2025-02-19 22:15:23 +1100

    n

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -42,8 +42,10 @@ export default ({ settings }: SettingsProps) => {
          />}
       />
       <FormInput value={settings.get('userid', '')} onTextChange={(text) => settings.set('userid', text)} />
-      <TouchableOpacity><div style={{padding: 30, backgroundColor: 'red'}}></div></TouchableOpacity>
       <Text>{settings.get('guild_list', '')}</Text>
       
    </>)
 };
+
+//<Text>{settings.get('pressed', 'no')}</Text>
+//<TouchableOpacity><div style={{padding: 30, backgroundColor: 'red'}}></div></TouchableOpacity>
